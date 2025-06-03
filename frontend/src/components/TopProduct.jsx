e21bf0a675f95ff61a8118e6e66049203cbaf396
Yash Bandooni <159815055+YashBandooni22@users.noreply.github.com>
2025-06-03 12:38:54 +0530

    Update TopProduct.jsx

--- frontend/src/components/TopProduct.jsx
+++ frontend/src/components/TopProduct.jsx
@@ -2,43 +2,43 @@ import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AppContext } from '../context/AppContext';
 
-import doc1 from '../assets/doc1.png';
-import doc2 from '../assets/doc2.png';
-import doc3 from '../assets/doc3.png';
-import doc4 from '../assets/doc4.png';
-import doc5 from '../assets/doc5.png';
-import doc6 from '../assets/doc6.png';
-import doc7 from '../assets/doc7.png';
-import doc8 from '../assets/doc8.png';
-import doc9 from '../assets/doc20.png';
-import doc10 from '../assets/doc21..png';
-import doc11 from '../assets/doc11.png';
-import doc12 from '../assets/doc12.png';
-import doc13 from '../assets/doc13.png';
-import doc14 from '../assets/doc14.png';
-import doc15 from '../assets/doc15.png';
+import item1 from '../assets/item1.png';
+import item2 from '../assets/item2.png';
+import item3 from '../assets/item3.png';
+import item4 from '../assets/item4.png';
+import item5 from '../assets/item5.png';
+import item6 from '../assets/item6.png';
+import item7 from '../assets/item7.png';
+import item8 from '../assets/item8.png';
+import item9 from '../assets/item20.png';
+import item10 from '../assets/item21..png';
+import item11 from '../assets/item11.png';
+import item12 from '../assets/item12.png';
+import item13 from '../assets/item13.png';
+import item14 from '../assets/item14.png';
+import item15 from '../assets/item15.png';
 
-const topDoctorImages = [
-  { _id: 'doc1', name: 'Basil Seeds', image: doc1, speciality: 'Seed' },
-  { _id: 'doc2', name: 'Potato Plant', image: doc2, speciality: 'Plant' },
-  { _id: 'doc3', name: 'Tomato', image: doc3, speciality: 'Plant' },
-  { _id: 'doc4', name: 'Corn Plant', image: doc4, speciality: 'Crop' },
-  { _id: 'doc5', name: 'Wheat', image: doc5, speciality: 'Plant' },
-  { _id: 'doc6', name: 'Banana Plant', image: doc6, speciality: 'Plant' },
-  { _id: 'doc7', name: 'Pumpkin Seeds', image: doc7, speciality: 'Seeds' },
-  { _id: 'doc8', name: 'NPK fertilizers', image: doc8, speciality: 'Fertilizer' },
-  { _id: 'doc20', name: 'Smallanthus', image: doc9, speciality: 'Plant' },
-  { _id: 'doc10', name: 'DDT', image: doc10, speciality: 'Pesticides' },
-  { _id: 'doc11', name: 'Sesame', image: doc11, speciality: 'Seed' },
-  { _id: 'doc12', name: 'Soya', image: doc12, speciality: 'Seeds' },
-  { _id: 'doc13', name: 'Dr. Zoe Kelly', image: doc13, speciality: 'Plant' },
-  { _id: 'doc14', name: 'Bamboo', image: doc14, speciality: 'Plant' },
-  { _id: 'doc15', name: 'Rice Plants', image: doc15, speciality: 'Plant' },
+const topitemtorImages = [
+  { _id: 'item1', name: 'Basil Seeds', image: item1, speciality: 'Seed' },
+  { _id: 'item2', name: 'Potato Plant', image: item2, speciality: 'Plant' },
+  { _id: 'item3', name: 'Tomato', image: item3, speciality: 'Plant' },
+  { _id: 'item4', name: 'Corn Plant', image: item4, speciality: 'Crop' },
+  { _id: 'item5', name: 'Wheat', image: item5, speciality: 'Plant' },
+  { _id: 'item6', name: 'Banana Plant', image: item6, speciality: 'Plant' },
+  { _id: 'item7', name: 'Pumpkin Seeds', image: item7, speciality: 'Seeds' },
+  { _id: 'item8', name: 'NPK fertilizers', image: item8, speciality: 'Fertilizer' },
+  { _id: 'item20', name: 'Smallanthus', image: item9, speciality: 'Plant' },
+  { _id: 'item10', name: 'DDT', image: item10, speciality: 'Pesticides' },
+  { _id: 'item11', name: 'Sesame', image: item11, speciality: 'Seed' },
+  { _id: 'item12', name: 'Soya', image: item12, speciality: 'Seeds' },
+  { _id: 'item13', name: 'Dr. Zoe Kelly', image: item13, speciality: 'Plant' },
+  { _id: 'item14', name: 'Bamboo', image: item14, speciality: 'Plant' },
+  { _id: 'item15', name: 'Rice Plants', image: item15, speciality: 'Plant' },
 ];
 
-const TopDoctors = () => {
+const Topitemtors = () => {
   const navigate = useNavigate();
-  const { doctors } = useContext(AppContext);
+  const { itemtors } = useContext(AppContext);
 
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
@@ -48,7 +48,7 @@ const TopDoctors = () => {
       </p>
 
       <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 pt-5 px-3 sm:px-0'>
-        {doctors.slice(0, 8).map((item, index) => (
+        {itemtors.slice(0, 8).map((item, index) => (
           <div
             key={index}
             onClick={() => {
@@ -60,7 +60,7 @@ const TopDoctors = () => {
             <div className='w-full h-48'>
               <img
                 className='w-full h-full object-cover'
-                src={topDoctorImages[index]?.image || item.image}
+                src={topitemtorImages[index]?.image || item.image}
                 alt={item.name}
               />
             </div>
@@ -72,9 +72,9 @@ const TopDoctors = () => {
                 <p>{item.available ? 'Available' : 'Not Available'}</p>
               </div>
               <p className='text-gray-900 text-base font-semibold leading-tight'>
-                {topDoctorImages[index]?.name}
+                {topitemtorImages[index]?.name}
               </p>
-              <p className='text-gray-600 text-sm leading-tight'>{topDoctorImages[index]?.speciality}</p>
+              <p className='text-gray-600 text-sm leading-tight'>{topitemtorImages[index]?.speciality}</p>
             </div>
           </div>
         ))}
@@ -82,7 +82,7 @@ const TopDoctors = () => {
 
       <button
         onClick={() => {
-          navigate('/doctors');
+          navigate('/itemtors');
           scrollTo(0, 0);
         }}
         className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'
@@ -93,4 +93,4 @@ const TopDoctors = () => {
   );
 };
 
-export default TopDoctors;
+export default Topitemtors;
